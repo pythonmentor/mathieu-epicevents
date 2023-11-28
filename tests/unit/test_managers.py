106740cfae3fcfe9998274b04f8d957cd05a33b3
tchappui <tchappui@gmail.com>
2023-11-28 08:40:16 +0100

    Exemple de test pour check_password

--- tests/unit/test_managers.py
+++ tests/unit/test_managers.py
@@ -1,28 +1,42 @@
-import pytest
-from tests import conftest
-from controllers.login_manager import AuthenticationAndPermissions
-from views.get_datas import GetDatas
+from unittest.mock import call
 
+from controllers import login_manager
 
-class MockResponse:
-    @staticmethod
-    def get_credentials():
-        email = "s@g.c"
-        password = "e"
-        return email, password
+FAKE_PASSWORD = "12345"
+FAKE_HASHED_PASSWORD = (
+    "$argon2id$v=19$m=65536,t=3,p=4$AcB4D8GYc27tnRPinNM6hw$2PfVv4W"
+    "H1CoD0/TWmyDw2z0iBTj++WAVt/G9IJRnvtg"
+)
 
 
 class TestLogin:
+    def test_check_password(self, mocker):
+        mocker.patch("controllers.login_manager.Menu")
+        mocker.patch("controllers.login_manager.Messages")
+        MockGetDatas = mocker.patch("controllers.login_manager.GetDatas")
+        MockGetDatas.return_value.get_credentials.return_value = (
+            "me@example.com",
+            FAKE_PASSWORD,
+        )
 
-    def test_check_password(self, monkeypatch):
-        def mock_get(*args, **kwargs):
-            return MockResponse()
+        mock_session = mocker.patch("controllers.login_manager.SESSION")
+        mock_staff_user = (
+            mock_session.query.return_value.filter.return_value.one_or_none.return_value
+        )
+        mock_staff_user.password = FAKE_HASHED_PASSWORD
+        mock_staff_user.department.name = "test_departement"
 
-        monkeypatch.setattr("views.get_datas.GetDatas", mock_get)
+        mock_create_token = mocker.patch(
+            "controllers.login_manager.AuthenticationAndPermissions.create_token"
+        )
+        MockMenuManager = mocker.patch("controllers.login_manager.MenuManager")
+        mock_choice_main_menu = MockMenuManager.return_value.choice_main_menu
 
-        sut = AuthenticationAndPermissions()
-        sut.check_password()
+        auth_and_perms = login_manager.AuthenticationAndPermissions()
+        auth_and_perms.check_password()
 
-        assert sut.menu.choice_main_menu()
-
-
+        # -tc- On vérifie que create_token est appelé avec les bons arguments
+        mock_create_token.assert_called_once_with("test_departement")
+        # -tc- On vérifie que choice_main_menu est appelé une fois et sans args
+        mock_choice_main_menu.assert_called_once()
+        mock_choice_main_menu.assert_called_once_with()
